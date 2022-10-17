94a5b4ad5e4552039709e8e0525bd3725e0f6e67
jpwalls08 <jpwalls08@outlook.com>
2022-10-17 14:40:36 -0500

    Check Point 1 (Complete)

---
+++ ToDoAPI/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ToDoAPI.Models.ToDoContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoDb"));
+    }
+    );
+
+//Step 05. Scaffold a new API controller using Entity Framework - Scaffold Categories choosing the Categories model, the ResourcesContext for DataContext. After walk thru the code in the controller and test using the browser (Swagger).
+//Looking for next step? Open ResourcesController.
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors();
+
+app.Run();
